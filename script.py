2d58480ccc78ba63de6be9cf08db559d06df3dec
Jace Browning <jacebrowning@gmail.com>
2021-04-24 10:37:54 -0400

    Update pomace

--- script.py
+++ script.py
@@ -5,10 +5,15 @@ URL = "https://www.gamestop.com/video-games/xbox-one/games/products/mlb-the-show
 settings = pomace.prompt("email")
 
 page = pomace.visit(URL, delay=2)
+
+pomace.log.info("Adding item to card")
 page = page.click_add_to_cart(wait=1)
 page = page.click_view_cart(delay=1)
+
+pomace.log.info("Proceeding to checkout")
 page = page.click_proceed_to_checkout(delay=2)
 
 page.fill_email(settings.email)
 
+pomace.log.warn("Starting debugger")
 breakpoint()
